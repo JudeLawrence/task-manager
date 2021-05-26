244910c805118ea6e2c57baa22305975966d0a5b
JudeLawrence <judesmail@gmail.com>
2021-05-26 14:43:58 +0400

    Added edit functionality for tasks

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'tasks#index'
 
+  # Create a task
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
+  # Update a task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # Show page for the task
   get 'tasks/:id', to: 'tasks#show', as: :task
 
 end
